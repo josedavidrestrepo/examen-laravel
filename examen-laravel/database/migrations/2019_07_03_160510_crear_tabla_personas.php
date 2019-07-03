b4d34baa1737ab43fb03878c6f685aafdca3224a
José David Restrepo Duque <jodrestreopdu@unal.edu.co>
2019-07-03 11:09:48 -0500

    Eliminar migraciones por defecto

---
+++ examen-laravel/database/migrations/2019_07_03_160510_crear_tabla_personas.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaPersonas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->string('numero_documento')->unique();
+            $table->string('tipo_documento')->unique();
+            $table->string('primer_nombre');
+            $table->string('segundo_nombre');
+            $table->string('primer_apellido');
+            $table->string('segundo_apellido');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personas');
+    }
+}
